671b6b5b80b2399e13fcf6d3a3b8c07fbec67573
Bueno-Italo <italo-bueno@hotmail.com>
2025-07-21 11:12:57 -0300

    Adicionando botões de limpar e fechar

--- src/GUI/frmCadastroPlataforma.Designer.cs
+++ src/GUI/frmCadastroPlataforma.Designer.cs
@@ -34,6 +34,8 @@
             this.txtNome = new System.Windows.Forms.TextBox();
             this.btnSalvar = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -41,6 +43,8 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.panel1.Controls.Add(this.btnFechar);
+            this.panel1.Controls.Add(this.btnLimpar);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.btnSalvar);
             this.panel1.Controls.Add(this.txtNome);
@@ -50,13 +54,14 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(776, 426);
             this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // lblTitulo
             // 
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitulo.ForeColor = System.Drawing.SystemColors.MenuBar;
-            this.lblTitulo.Location = new System.Drawing.Point(21, 34);
+            this.lblTitulo.Location = new System.Drawing.Point(232, 28);
             this.lblTitulo.Name = "lblTitulo";
             this.lblTitulo.Size = new System.Drawing.Size(283, 32);
             this.lblTitulo.TabIndex = 0;
@@ -70,9 +75,9 @@
             this.lblNome.ForeColor = System.Drawing.SystemColors.MenuBar;
             this.lblNome.Location = new System.Drawing.Point(24, 87);
             this.lblNome.Name = "lblNome";
-            this.lblNome.Size = new System.Drawing.Size(163, 21);
+            this.lblNome.Size = new System.Drawing.Size(173, 21);
             this.lblNome.TabIndex = 1;
-            this.lblNome.Text = "Nome da Platafrma:";
+            this.lblNome.Text = "Nome da Plataforma:";
             // 
             // txtNome
             // 
@@ -83,23 +88,50 @@
             // 
             // btnSalvar
             // 
-            this.btnSalvar.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSalvar.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.btnSalvar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnSalvar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.ForeColor = System.Drawing.Color.White;
             this.btnSalvar.Location = new System.Drawing.Point(28, 125);
             this.btnSalvar.Name = "btnSalvar";
             this.btnSalvar.Size = new System.Drawing.Size(106, 32);
             this.btnSalvar.TabIndex = 3;
             this.btnSalvar.Text = "Salvar";
             this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(140, 137);
+            this.pictureBox1.Location = new System.Drawing.Point(28, 28);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(28, 20);
+            this.pictureBox1.Size = new System.Drawing.Size(106, 44);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnLimpar.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.ForeColor = System.Drawing.Color.White;
+            this.btnLimpar.Location = new System.Drawing.Point(155, 125);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(106, 32);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnFechar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.ForeColor = System.Drawing.Color.White;
+            this.btnFechar.Location = new System.Drawing.Point(280, 125);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(106, 32);
+            this.btnFechar.TabIndex = 6;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = false;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
             // 
             // frmCadastroPlataforma
             // 
@@ -109,6 +141,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panel1);
             this.Name = "frmCadastroPlataforma";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmCadastroPlataforma";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -125,5 +158,7 @@
         private System.Windows.Forms.Label lblNome;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnFechar;
     }
 }
--- src/GUI/frmCadastroPlataforma.cs
+++ src/GUI/frmCadastroPlataforma.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GUI.BLL;
+using GUI.DAL;
+using GUI.Modelo;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +18,42 @@ namespace GUI
         public frmCadastroPlataforma()
         {
             InitializeComponent();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Plataforma modelo = new Plataforma();
+                modelo.Nome = txtNome.Text.Trim();
+
+                DAL.Conexao conn = new Conexao();
+                BLLPlataforma bll = new BLLPlataforma(conn);
+                bll.Inserir(modelo);
+
+                MessageBox.Show("plataforma cadastrada com sucesso!");
+                txtNome.Clear();
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNome.Clear();
+            txtNome.Focus();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close(); //Fecha form atual
         }
     }
 }
